df903c21c2698e70fcfb4cd0049d40981e34cac4
Gielen Robin <robin.gielen@student.uclouvain.be>
2015-04-23 17:37:36 +0200

    toString - Node
    First commit, no test has been done so far

--- Node.java
+++ Node.java
@@ -73,7 +73,59 @@ class Node implements ExprIF{
      *       complètement parenthésée de l'arbre  
      */
     public String toString(){
-        return "lol";
+        
+        Node n = this;
+        
+        String stringTreated = "";
+        boolean littleTreeUnfinished = true;
+        boolean fromLeft = true;
+        boolean beOnRight = false;
+        int rightCounter = 0;
+        
+        while (n.left != null && n.right != null)
+        {
+            while (littleTreeUnfinished)
+            {
+                while (n.left != null)
+                {
+                    beOnRight = false;
+                    n = n.left;
+                }
+                if (!beOnRight)
+                {
+                    stringTreated = "(" + n.carac + "" + n.back.carac + stringTreated;
+                    n = n.back.right;
+                }
+                if (beOnRight)
+                {
+                    rightCounter = 0;
+                    stringTreated = "(" + stringTreated;
+                    stringTreated = stringTreated + "" + n.back.carac + "" + n.carac +")";
+                    n = n.back.back;
+                    littleTreeUnfinished = false;
+                }
+                if (n.left == null && !beOnRight)
+                {
+                    stringTreated = stringTreated + "" + n.carac + ")";
+                    n = n.back; 
+                    littleTreeUnfinished = false;
+                }
+                else if (!beOnRight)
+                {
+                    rightCounter++;
+                }
+            }
+            for (int i = 0; i < rightCounter + 1; i++)
+            {
+                stringTreated = stringTreated + ")";
+                n = n.back;
+            }
+            n = n.right;
+            beOnRight = true;
+            littleTreeUnfinished = true;
+        }
+        
+        return stringTreated;
     }
     
     /**
